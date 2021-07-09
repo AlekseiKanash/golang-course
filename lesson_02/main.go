7de3e24f898860c5653dd928840b67e470aff153
Aleksei Kanash <aleksei.kanash@gmail.com>
2021-07-09 16:40:11 +0300

    lesson_02 more understandable sequence of calls in the main function.

--- lesson_02/main.go
+++ lesson_02/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	defer fmt.Print("Done.\n")
 	fmt.Printf("Fibonacci numbers are calculating...\n")
+	defer fmt.Print("Done.\n")
 	fibonacci.PrintFibonacciSequence(10)
 }
