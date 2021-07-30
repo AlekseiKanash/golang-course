98877daa41059c2686255f40014f6d8550fa837e
Aleksei Kanash <aleksei.kanash@gmail.com>
2021-07-31 01:47:29 +0300

    lesson_06 task 3 extra code removed

--- lesson_06/task_03/main.go
+++ lesson_06/task_03/main.go
@@ -1,35 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
 )
 
-var validPath = regexp.MustCompile("^/(edit|save|view|user|saveUser)/([a-zA-Z0-9]+)$")
+var validPath = regexp.MustCompile("^/(user)/([a-zA-Z0-9]+)$")
 var templates = template.Must(template.ParseFiles("user.html"))
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("invalid Page Title")
-	}
-	return m[2], nil // The title is the second subexpression.
-}
-
-func loadUserPage(title string) ([]byte, error) {
-	filename := title + ".html"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
-}
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p interface{}) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
